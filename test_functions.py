579cc5f997c240f9e264a329aefbc60964ad0023
richb999git <37702953+richb999git@users.noreply.github.com>
2018-03-23 15:02:43 +0000

    Add files via upload

---
+++ test_functions.py
@@ -0,0 +1,26 @@
+def hello_func():
+    return "Hello function"
+
+def hello_func2(greeting="Howdy"):
+    return "{} Function".format(greeting)
+
+def student_info(*args, **kwargs):     # * means single bits of data
+    print(args)                        # ** means two bits (key words) (dictionary)
+    print(kwargs)                      # args and kwargs is convention
+
+print(hello_func().upper())
+
+print(hello_func2("Hi").lower())
+
+print(hello_func2().lower())
+
+student_info("Maths", "Art", name = "John Barn", Age = 21, student_num = 25764)
+# this produces a tuple containing Maths and Art
+# and a dictionary containing name, age, student number
+
+courses = ["History", "Physics"]
+info = {"name":"Dave glut", "Age":20, "student_num": 83957}
+
+student_info(courses, info)    #not what we wanted  (first 2 "fields" populated only)
+student_info(*courses, **info)    #* and ** make it treat it as we want
+
